568bda2b4759901ea4a4a5498140ee46c28cb41f
apdastous <apdastous@gmail.com>
2023-11-16 23:05:50 -0600

    latest_commands.py won't write the same content if unchanged

--- latest_commands.py
+++ latest_commands.py
@@ -4,15 +4,18 @@ import redis
 
 if __name__ == '__main__':
     r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+    previous = None
     while True:
         latest = r.zrevrange('latest_commands', 0, 19)
-        with open("latest_commands.txt", "w") as scratch_file:
-            for action in latest:
-                username = action.split('#')[0]
-                display = action.split('#')[1]
+        if latest != previous:
+            with open("latest_commands.txt", "w") as scratch_file:
+                for action in latest:
+                    username = action.split('#')[0]
+                    display = action.split('#')[1]
 
-                scratch_file.write(f'{username}: {display}\n')
+                    scratch_file.write(f'{username}: {display}\n')
 
-        scratch_file.close()
+            scratch_file.close()
+            previous = latest
 
         sleep(1)
